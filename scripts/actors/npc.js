5cd526a65f8ac6cc45a90f041c486c07406c0765
Saibot393 <137942782+Saibot393@users.noreply.github.com>
2026-09-06 22:25:14 +0200

    add lootable setting

--- scripts/actors/npc.js
+++ scripts/actors/npc.js
@@ -9,12 +9,19 @@ export class o13npcActor extends inventoryActor {
 			description : this.system.description
 		}
 	}
+	
+	//looting
+	get lootable() {
+		return this.system.lootable;
+	}
 }
 
 export class npcDataModel extends foundry.abstract.TypeDataModel {
 	static defineSchema() {
 		return {
-			description: new HTMLField({ required: true, blank: true, initial: "" })
+			description: new HTMLField({ required: true, blank: true, initial: "" }),
+			
+			lootable : new BooleanField({ required: true, initial: false})
 		};
 	}
 }
